bc75f0020a55f9513672f87586c6b87fc0224f83
Hadrien Reynders <reynders.hadrien@gmail.com>
2022-02-15 15:14:47 +0100

    all task done but not working yet

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+50.times do
+  task = Task.new(title: Faker::Name.unique.name,
+              details: Faker::Lorem.sentence,
+              completed: rand(2) == 1 ? true : false )
+  task.save
+end
