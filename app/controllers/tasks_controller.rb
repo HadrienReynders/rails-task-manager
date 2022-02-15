3da5d3290bfe8c65d6bf2196c579ecb9a92f0f7a
Hadrien Reynders <reynders.hadrien@gmail.com>
2022-02-15 16:21:52 +0100

    final without checkbox and with simple front end

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,7 +12,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(params[:task])
+    @task = Task.new(task_params)
     @task.save
 
     redirect_to task_path(@task)
@@ -24,7 +24,7 @@ class TasksController < ApplicationController
 
   def update
     @task = Task.find(params[:id])
-    @task.update(params[:task])
+    @task.update(task_params)
 
     redirect_to task_path(@task)
   end
@@ -33,12 +33,12 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
     @task.destroy
 
-    redirect_to task_path(@task)
+    redirect_to tasks_path
   end
 
   private
 
   def task_params
-    params.require(:task).permit(:title, :details)
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
